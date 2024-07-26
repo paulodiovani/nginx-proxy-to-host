15245805da714b138ce8ce50abf91d670b92b420
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2024-07-26 15:12:53 -0300

    Add grpc config and create start script

---
+++ scripts/start_nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export DOLLAR='$'
+
+set -e 
+
+if [ -n "$GRPC" ]; then
+  envsubst < /tmp/nginx-grpc.conf.template > /etc/nginx/conf.d/default.conf
+else
+  envsubst < /tmp/nginx-http.conf.template > /etc/nginx/conf.d/default.conf
+fi
+
+cat /etc/nginx/conf.d/default.conf
+
+nginx -g 'daemon off;'
